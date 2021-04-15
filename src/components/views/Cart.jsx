eeb348f32acbedeaf0ac00a1df4c7f2cda834aa3
hkgnp <ben@pngs.cc>
2021-04-16 01:14:09 +0800

    Implemented computation for shopping cart

--- src/components/views/Cart.jsx
+++ src/components/views/Cart.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { config } from '../../config';
 import { UserContext } from '../../Context';
-import { Col } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
+import loadingImage from '../../rolling.svg';
 
 const Cart = () => {
   const userContext = useContext(UserContext);
@@ -17,17 +18,38 @@ const Cart = () => {
       );
       setCart(response.data);
       setLoaded(true);
+      console.log(response.data.allItems);
     })();
   }, [userContext]);
+
+  const getTotalCost = () => {
+    let totalCost = cart.allItems.map(
+      (p) => (p.products.cost * p.quantity) / 100
+    );
+    return totalCost.reduce((a, b) => a + b, 0);
+  };
 
   return (
     <Col>
       <h1>Your Shopping Cart</h1>
+      <Row>
+        {loaded === false && (
+          <img
+            className="loading-image mt-5"
+            src={loadingImage}
+            alt="Loading spinner"
+          />
+        )}
+      </Row>
       <div>
         {loaded === true &&
           cart.allItems.map((p) => (
-            <li key={p.products.id}>{p.products.name}</li>
+            <li key={p.products.id}>
+              {p.products.name}, {p.quantity}, ${p.products.cost / 100}, $
+              {(p.products.cost / 100) * p.quantity}
+            </li>
           ))}
+        {loaded === true && `$${getTotalCost()}`}
       </div>
     </Col>
   );

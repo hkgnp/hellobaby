a1ee4cc6d42bd8743202d37d03c66e9dd45c76b6
hkgnp <ben@pngs.cc>
2021-04-16 21:21:47 +0800

    Minor changes to ABout This App page

--- src/components/views/About.jsx
+++ src/components/views/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Col } from 'reactstrap';
 
 const About = () => {
-  return <h1>About This App</h1>;
+  return (
+    <Col>
+      <h1>About This App</h1>
+    </Col>
+  );
 };
 
 export default About;

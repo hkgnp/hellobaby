47b9f2b3be1d4f6b3240a463ac2bf8411a33a550
hkgnp <ben@pngs.cc>
2021-04-16 11:27:44 +0800

    Css changes to quantity input field

--- src/components/views/Cart.jsx
+++ src/components/views/Cart.jsx
@@ -10,6 +10,7 @@ const Cart = () => {
 
   const [cart, setCart] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [quantity, setQuantity] = useState({});
 
   useEffect(() => {
     (async () => {
@@ -18,9 +19,16 @@ const Cart = () => {
       );
       setCart(response.data);
       console.log(response.data.allItems);
-      await setLoaded(true);
+      setLoaded(true);
     })();
   }, [userContext]);
+
+  const handleQuantity = (e) => {
+    alert(`Quantity of ${e.target.name} changed to ${e.target.value}`);
+    setQuantity({
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const getTotalCost = () => {
     let totalCost = cart.allItems.map(
@@ -31,7 +39,7 @@ const Cart = () => {
 
   return (
     <Col>
-      <h1>Your Shopping Cart</h1>
+      <h2>Shopping Cart</h2>
       <Row>
         {loaded === false && (
           <img
@@ -45,14 +53,14 @@ const Cart = () => {
         {loaded === true &&
           cart.allItems.map((p) => (
             <Row key={p.products.id}>
-              <Col className="border col-4 p-0 m-0 d-flex align-items-center">
+              <Col className="col-4 p-0 m-0 d-flex align-items-center">
                 <img
                   className="cart-display-img"
                   alt={p.products.name}
                   src={p.products.thumbnail_url}
                 />
               </Col>
-              <Col className="border cart-display">
+              <Col className="cart-display">
                 <h4 className="m-0">{p.products.name}</h4>
                 <p className="m-0" style={{ color: '#E1084F' }}>
                   ${p.products.cost / 100}
@@ -61,24 +69,39 @@ const Cart = () => {
                   className="form-group col-4 p-0 d-flex m-0"
                   style={{ height: '20px' }}
                 >
-                  <button className="btn btn-sm p-2 m-0">-</button>
+                  <button
+                    onClick={handleDecrement}
+                    className="btn btn-sm p-2 m-0"
+                  >
+                    -
+                  </button>
                   <input
                     value={p.quantity}
                     type="text"
                     size="1"
-                    className="form-control p-1"
+                    className="form-control quantity-input"
+                    name={p.id}
+                    onChange={handleQuantity}
                   />
-                  <button className="btn btn-sm p-2 m-0">+</button>
+                  <button
+                    onClick={handleIncrement}
+                    className="btn btn-sm p-2 m-0"
+                  >
+                    +
+                  </button>
                 </div>
               </Col>
             </Row>
           ))}
       </div>
-      <Row className="border d-flex justify-content-between align-items-center p-1">
+      <Row className="d-flex justify-content-between align-items-center p-1">
         <h5 className="m-0 p-0">
-          Total: <span style={{ color: '#E1084F' }}>${getTotalCost()}</span>
+          Total:{' '}
+          <span style={{ color: '#E1084F' }}>
+            ${loaded === true && getTotalCost()}
+          </span>
         </h5>
-        <button className="btn btn-primary btn-sm">Checkout</button>
+        <button className="btn-sm">Checkout</button>
       </Row>
     </Col>
   );

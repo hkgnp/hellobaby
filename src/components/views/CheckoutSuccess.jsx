b20d7abc4d54edfc023fbb5dba5f770ecf874342
hkgnp <ben@pngs.cc>
2021-04-17 12:11:24 +0800

    Implemented order confirmation

--- src/components/views/CheckoutSuccess.jsx
+++ src/components/views/CheckoutSuccess.jsx
@@ -1,23 +1,54 @@
 import axios from 'axios';
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { config } from '../../config';
 import { UserContext } from '../../Context';
 import { Col } from 'reactstrap';
 
 const CheckoutSuccess = () => {
   const userContext = useContext(UserContext);
+  const sessionId = window.location.href.substring(
+    window.location.href.lastIndexOf('=%20') + 4
+  );
+
+  const [purchasedItems, setPurchasedItems] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const response = await axios.get(
+      // Route to clear shopping cart
+      const checkoutResponse = await axios.get(
         `${config.BASE_URL}/api/checkout/success/${userContext.user().id}`
       );
-      console.log(response.data);
+      console.log(checkoutResponse.data);
+
+      // Route to get line items from Stripe checkout session
+      const stripeResponse = await axios.get(
+        `https://api.stripe.com/v1/checkout/sessions/${sessionId}/line_items`,
+        {
+          headers: {
+            Authorization:
+              'Bearer ' +
+              'sk_test_51IaGU3L2CSs7osvhVdiPNeh3D3oVJrbqwUiqSe3n0uojPpADzO709W3KAerXNX5A7GIlPfOBtfqjCoIWFiE2rwLB00dp6wOUNv',
+          },
+        }
+      );
+      setPurchasedItems(stripeResponse.data.data);
     })();
-  });
+  }, [sessionId, userContext]);
+
+  const getTotalCost = () => {
+    let totalCost = purchasedItems.map((i) => i.amount_subtotal / 100);
+    return totalCost.reduce((a, b) => a + b, 0);
+  };
+
   return (
     <Col>
-      <h1>Thank you for your purchase!</h1>
+      <h1>Order Confirmed</h1>
+      {purchasedItems.map((i) => (
+        <li>
+          {i.description}, ${i.amount_subtotal / 100}, {i.quantity}
+        </li>
+      ))}
+      <span>${getTotalCost()}</span>
     </Col>
   );
 };
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const config = {
   BASE_URL: 'https://31e3d35c2a1f.ngrok.io',
+  STRIPE_API_URL: 'https://api.stripe.com/v1',
 };

e6639fe37db1d5b6735883ddcfa5dc6a5ee91588
hkgnp <ben@pngs.cc>
2021-04-18 21:45:33 +0800

    Implemented Joi validation for user registration

---
+++ src/components/common/ValidateUser.jsx
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import Joi from 'joi';
+import { config } from '../../config';
+
+const ValidateUser = async (props) => {
+  // Destructure
+  const {
+    username,
+    email,
+    password,
+    confirmPassword,
+    address,
+    postalCode,
+    role,
+  } = props;
+
+  // Set up schema for Joi
+  const schema = Joi.object({
+    username: Joi.string().required().label('Name'),
+    email: Joi.string()
+      .required()
+      .email({ minDomainSegments: 2, tlds: { allow: false } })
+      .label('Username'),
+    password: Joi.string().required().min(5).label('Password'),
+    confirmPassword: Joi.any()
+      .equal(Joi.ref('password'))
+      .required()
+      .label('Passwords')
+      .messages({ 'any.only': 'Passwords do not match' }),
+    address: Joi.string().required().label('Address'),
+    postalCode: Joi.string().min(6).max(6).required().label('Postal Code'),
+  });
+
+  // Implement Joi validation
+  const validationResult = schema.validate(
+    { username, email, password, confirmPassword, address, postalCode },
+    {
+      abortEarly: false,
+    }
+  );
+
+  if (validationResult.error !== null) {
+    const errors = {};
+    validationResult.error.details.map((e) => (errors[e.path[0]] = e.message));
+    return errors;
+  } else {
+    try {
+      await axios.post(`${config.BASE_URL}/api/users/register`, {
+        username: username,
+        email: email,
+        password: password,
+        address: address,
+        postal_code: postalCode,
+        role_id: role,
+      });
+      return 'User successfully registered';
+    } catch (e) {
+      return 'Username has already been taken';
+    }
+  }
+};
+
+export default ValidateUser;
--- src/components/views/Register.jsx
+++ src/components/views/Register.jsx
@@ -1,39 +1,50 @@
-import axios from 'axios';
 import React, { useState } from 'react';
 import { Col, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import { config } from '../../config';
+import ValidateUser from '../common/ValidateUser';
 
 const Register = (props) => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [address, setAddress] = useState('');
   const [postalCode, setPostalCode] = useState('');
+  const [errors, setErrors] = useState({});
   const [role] = useState(2);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios.post(`${config.BASE_URL}/api/users/register`, {
-      username: username,
-      email: email,
-      password: password,
-      address: address,
-      postal_code: postalCode,
-      role_id: role,
+    const errorMessages = await ValidateUser({
+      username,
+      email,
+      password,
+      confirmPassword,
+      address,
+      postalCode,
+      role,
     });
+    console.log(errorMessages);
 
-    const tokenResponse = await axios.post(
-      `${config.BASE_URL}/api/users/login`,
-      {
-        email: email,
-        password: password,
-      }
-    );
-    localStorage.setItem('accessToken', tokenResponse.data.accessToken);
-    localStorage.setItem('refreshToken', tokenResponse.data.refreshToken);
+    if (errorMessages === 'Username has already been taken') {
+      setErrors({
+        usernameTaken: 'Username has already been taken',
+      });
+    } else {
+      setErrors(errorMessages);
+    }
 
-    // Redirect
-    window.location.href = '/allproducts';
+    // const tokenResponse = await axios.post(
+    //   `${config.BASE_URL}/api/users/login`,
+    //   {
+    //     email: email,
+    //     password: password,
+    //   }
+    // );
+    // localStorage.setItem('accessToken', tokenResponse.data.accessToken);
+    // localStorage.setItem('refreshToken', tokenResponse.data.refreshToken);
+
+    // // Redirect
+    // window.location.href = '/allproducts';
   };
 
   return (
@@ -49,6 +60,9 @@ const Register = (props) => {
             placeholder="Your fine name"
             onChange={(e) => setUsername(e.target.value)}
           />
+          {errors.username ? (
+            <div className="alert-sm alert-danger p-2">{errors.username}</div>
+          ) : null}
         </FormGroup>
         <FormGroup>
           <Label>Email</Label>
@@ -58,6 +72,9 @@ const Register = (props) => {
             placeholder="Your email"
             onChange={(e) => setEmail(e.target.value)}
           />
+          {errors.email ? (
+            <div className="alert-sm alert-danger p-2">{errors.email}</div>
+          ) : null}
         </FormGroup>
         <FormGroup>
           <Label>Password</Label>
@@ -67,6 +84,9 @@ const Register = (props) => {
             placeholder="Enter a complex password"
             onChange={(e) => setPassword(e.target.value)}
           />
+          {errors.password ? (
+            <div className="alert-sm alert-danger p-2">{errors.password}</div>
+          ) : null}
         </FormGroup>
         <FormGroup>
           <Label>Confirm Password</Label>
@@ -74,7 +94,13 @@ const Register = (props) => {
             type="password"
             name="confirm-password"
             placeholder="Confirm your password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
+          {errors.confirmPassword ? (
+            <div className="alert-sm alert-danger p-2">
+              {errors.confirmPassword}
+            </div>
+          ) : null}
         </FormGroup>
         <FormGroup>
           <Label>Address</Label>
@@ -84,6 +110,9 @@ const Register = (props) => {
             placeholder="Delivery address please"
             onChange={(e) => setAddress(e.target.value)}
           />
+          {errors.address ? (
+            <div className="alert-sm alert-danger p-2">{errors.address}</div>
+          ) : null}
           <FormText color="muted">
             Apologies, we currently only ship within Singapore.
           </FormText>
@@ -96,6 +125,9 @@ const Register = (props) => {
             placeholder="SG postal code"
             onChange={(e) => setPostalCode(e.target.value)}
           />
+          {errors.postalCode ? (
+            <div className="alert-sm alert-danger p-2">{errors.postalCode}</div>
+          ) : null}
         </FormGroup>
         <button className="btn btn-success btn-sm mr-2">Submit</button>
         <a href="/allproducts" className="btn btn-danger btn-sm">

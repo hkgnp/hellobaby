834b38324fc2f67cdf439a7e04e76df626c1af6d
hkgnp <ben@pngs.cc>
2021-04-23 15:53:26 +0800

    Added logos to footer

--- src/components/views/Footer.jsx
+++ src/components/views/Footer.jsx
@@ -5,14 +5,24 @@ const Footer = () => {
   return (
     <Col className="footer">
       <Col className="footer-text">
-        HelloBaby 2021
+        HelloBaby &copy; 2021
         <br />
         Inc in Singapore
-        <br />5 Jalan Longkang
         <br />
-        Singapore 100100
+        35 Orchard Road
+        <br />
+        Singapore 238823
       </Col>
-      <Col className="footer-text">Column 2</Col>
+      <Col className="footer-logo">
+        {/* Follow Us:
+        <br /> */}
+        <i class="fab fa-twitter"></i>&nbsp;&nbsp;&nbsp;
+        <i class="fab fa-facebook"></i>
+        &nbsp;&nbsp;&nbsp;<i class="fab fa-instagram"></i>&nbsp;&nbsp;&nbsp;
+        <i class="fab fa-pinterest-p"></i>&nbsp;&nbsp;&nbsp;
+        <i class="fab fa-tiktok"></i>&nbsp;&nbsp;&nbsp;
+        <i class="fab fa-youtube"></i>
+      </Col>
     </Col>
   );
 };
